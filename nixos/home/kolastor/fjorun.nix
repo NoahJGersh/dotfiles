f5a671c0dde6cc1a83b0229788153f566dd39691
NoahJGersh <me@noahger.sh>
2025-11-24 22:02:13 -0800

    fix(fjorun): use correct niri mode syntax

--- nixos/home/kolastor/fjorun.nix
+++ nixos/home/kolastor/fjorun.nix
@@ -8,13 +8,21 @@
 
   # LG Ultrawide
   programs.niri.settings.outputs."DP-1" = {
-    mode = "2560x1080@144.001";
+    mode = {
+      width = 2560;
+      height = 1080;
+      refresh = 144.001;
+    };
     variable-refresh-rate = "on-demand";
     focus-at-startup = true;
   };
 
   # Dell 16:9
   programs.niri.settings.outputs."HDMI-A-1" = {
-    mode = "2560x1440@144.000";
+    mode = {
+      width = 2560;
+      height = 1440;
+      refresh = 144.000;
+    };
   };
 }
