723b1c366316855b848b57b817debb5314561552
NoahJGersh <me@noahger.sh>
2025-11-24 21:58:53 -0800

    fix(mako): use hardcoded font until proper config is done

--- nixos/home/kolastor/features/desktop/common/wayland/mako.nix
+++ nixos/home/kolastor/features/desktop/common/wayland/mako.nix
@@ -2,7 +2,8 @@
 {
   services.mako.enable = true;
   services.mako.settings  = {
-    font = "${config.fontProfiles.regular.name} ${toString config.fontProfiles.regular.size}";
+    # font = "${config.fontProfiles.regular.name} ${toString config.fontProfiles.regular.size}";
+    font = "FiraCode Nerd Font 10";
     "app-name=Spotify" = {
       invisible = true;
     };
