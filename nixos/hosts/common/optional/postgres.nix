e79ad53bb65beebbbc2cadef8d065453b329332d
Noah Gersh <me@noahger.sh>
2026-02-26 20:29:02 -0800

    chore(postgres): add TCP/IP connection

--- nixos/hosts/common/optional/postgres.nix
+++ nixos/hosts/common/optional/postgres.nix
@@ -2,9 +2,15 @@
 {
   config.services.postgresql = {
     enable = true;
+    enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
+      #type  database  DBuser  origin-address  auth-method
+      local  all       all                     trust
+      
+      #ipv4
+      host   all       all     127.0.0.1/32    trust
+      #ipv6
+      host   all       all     ::1/128         trust
     '';
   };
 }
